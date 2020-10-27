4a9bf59764bffc5ad49627c7e63d126f5d485d83
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2020-10-27 13:49:07 -0400

    Update EvaluationService.java
    First attempt at phone number test.

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -200,8 +200,18 @@ public class EvaluationService {
 	 * NANP-countries, only 1 is considered a valid country code.
 	 */
 	public String cleanPhoneNumber(String string) {
-		// TODO Write an implementation for this method declaration
-		return null;
+		
+		string = string.replaceAll("[^0-9]", "");
+		
+		if(string.startsWith("1")) {
+			string = string.substring(1);
+		}
+		
+		if(string.length() != 10) {
+			throw new IllegalArgumentException("Invaild entry");
+		}
+
+		return string;
 	}
 
 	/**
